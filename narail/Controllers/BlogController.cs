4d59b5d10cb54bddbaeacf437bd3ced453ff3ca7
serkansrkn <55769261+serkansrkn@users.noreply.github.com>
2019-10-16 23:13:53 +0300

    Add project files.

---
+++ narail/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace narail.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Blog";
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Popular()
+        {
+            return View();
+        }
+
+        public ActionResult Tag()
+        {
+            return View();
+        }
+
+    }
+}
