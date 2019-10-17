216d4a33db834457802ffcff6c702a237b10a9da
serkansrkn <55769261+serkansrkn@users.noreply.github.com>
2019-10-17 19:04:56 +0300

    iletişim eklendi

---
+++ narail/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace narail.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Title = "İletişim";
+            return View();
+        }
+    }
+}
