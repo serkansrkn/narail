cdbacb00d7e3fdcd5119adfde9db5e31bc40cc9d
serkansrkn <55769261+serkansrkn@users.noreply.github.com>
2019-10-24 06:17:06 +0300

    Düzenleme

---
+++ narail/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace narail.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Proje";
+            return View();
+        }
+    }
+}
